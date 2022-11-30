0715ecd7a21974ac874c3ff0583a37749ae14e7e
Abeds macbook pro <abedharirii@gmail.com>
2022-11-30 23:35:44 +0100

    moved user-service into CPHMTOGO folder

---
+++ CPHMTOGO/user-service/src/controllers/customer.controller.ts
@@ -0,0 +1,44 @@
+import { v4 as uuid } from "uuid";
+import { ICustomer } from "../interfaces/customer.interface";
+import mapper from "../models/customer.model";
+
+function createCustomer(customer: ICustomer) {
+	return mapper.createCustomer({
+		id: uuid(),
+		firstname: customer.firstname,
+		lastname: customer.lastname,
+		phone: customer.phone,
+		birtdate: new Date(customer.birtdate),
+		address: customer.address,
+		loginInfoId: customer.loginInfoId,
+		role: customer.role,
+	});
+}
+
+function updateCustomer(customer: ICustomer, id: string) {
+	return mapper.updateCustomer({
+		id,
+		firstname: customer.firstname,
+		lastname: customer.lastname,
+		phone: customer.phone,
+		birtdate: new Date(customer.birtdate),
+		address: customer.address,
+		loginInfoId: customer.loginInfoId,
+		role: customer.role,
+	});
+}
+
+function getCustomerById(id: string) {
+	return mapper.getCustomerById(id);
+}
+
+function deleteCustomer(id: string) {
+	return mapper.deletCustomer(id);
+}
+
+export default {
+	createCustomer,
+	updateCustomer,
+	deleteCustomer,
+	getCustomerById,
+};
