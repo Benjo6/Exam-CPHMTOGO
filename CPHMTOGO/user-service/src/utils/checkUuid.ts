c40ab4eed722d2dd5244ef745f874c58c48b0429
Abeds macbook pro <abedharirii@gmail.com>
2022-11-30 23:37:34 +0100

    deleted wrong folder

--- CPHMTOGO/user-service/src/utils/checkUuid.ts
+++
@@ -1,5 +0,0 @@
-export default function isValidUuid(uuid: string) {
-  const regexExp =
-    /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
-  return regexExp.test(uuid);
-}
