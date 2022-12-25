7e7d732b20c9148029f34e6d9f130d04f53e6b39
Abeds macbook pro <abedharirii@gmail.com>
2022-12-25 12:40:00 +0100

    fixed init.sql files for users and restaurants

--- Infrastructure/docker/init/users/init.sql
+++ Infrastructure/docker/init/users/init.sql
@@ -2,10 +2,10 @@ CREATE TABLE
   public."Company" (
     id uuid NOT NULL,
     name text NOT NULL,
-    role text NOT NULL DEFAULT 'Admin' ::text,
-    "loginInfoId" text NOT NULL,
+    role text NOT NULL,
     "kontoNr" integer NOT NULL,
-    "regNr" integer NOT NULL
+    "regNr" integer NOT NULL,
+    "loginInfoId" uuid NOT NULL
   );
 
 ALTER TABLE
@@ -20,9 +20,9 @@ CREATE TABLE
     lastname text NOT NULL,
     phone integer NOT NULL,
     birtdate timestamp(3) without time zone NOT NULL,
-    address text NOT NULL,
-    "loginInfoId" text NOT NULL,
-    role text NOT NULL DEFAULT 'Customer' ::text
+    role text NOT NULL,
+    address uuid NOT NULL,
+    "loginInfoId" uuid NOT NULL
   );
 
 ALTER TABLE
@@ -37,13 +37,14 @@ CREATE TABLE
     lastname text NOT NULL,
     active boolean NOT NULL,
     "loginInfoId" uuid NOT NULL,
-    address text NOT NULL,
-    role text NOT NULL DEFAULT 'Employee' ::text,
+    role text NOT NULL,
     "kontoNr" integer NOT NULL,
-    "regNr" integer NOT NULL
+    "regNr" integer NOT NULL,
+    "accountId" text NOT NULL,
+    address uuid NOT NULL
   );
 
 ALTER TABLE
   public."Employee"
 ADD
-  CONSTRAINT "Employee_pkey" PRIMARY KEY (id)
+  CONSTRAINT "Employee_pkey" PRIMARY KEY (id);
