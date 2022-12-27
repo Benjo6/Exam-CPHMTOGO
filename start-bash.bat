9a94899bb1a5206a65ebca59a92fae27854cfa26
Benjo6 <58556176+Benjo6@users.noreply.github.com>
2022-12-27 01:49:07 +0100

    Update start-bash.bat

--- start-bash.bat
+++ start-bash.bat
@@ -1,5 +1,2 @@
 docker compose -f infrastructure/docker/docker-compose.infrastructure.prod.yml down --remove-orphans
-docker compose -f infrastructure/docker/docker-compose.applications.dev.yml down --remove-orphans
-
 docker compose -f infrastructure/docker/docker-compose.infrastructure.prod.yml up --build -d
-docker compose -f infrastructure/docker/docker-compose.applications.dev.yml up --build -d
